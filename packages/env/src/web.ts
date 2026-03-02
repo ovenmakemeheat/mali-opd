a99dd504129d8127290a401de79a45af5aef2edc
N0TAW00D <primsirikunawutnatthawat@gmail.com>
2026-03-02 15:19:11 +0700

    add: server+db+orm+auth+env

--- packages/env/src/web.ts
+++ packages/env/src/web.ts
@@ -2,5 +2,11 @@ import { createEnv } from "@t3-oss/env-nextjs";
 import { z } from "zod";
 
 export const env = createEnv({
-  emptyStringAsUndefined: true,
+	client: {
+		NEXT_PUBLIC_SERVER_URL: z.url(),
+	},
+	runtimeEnv: {
+		NEXT_PUBLIC_SERVER_URL: process.env.NEXT_PUBLIC_SERVER_URL,
+	},
+	emptyStringAsUndefined: true,
 });
