7fa89ec5160ecba32a4b42308a28868cc065f93e
N0TAW00D <primsirikunawutnatthawat@gmail.com>
2026-03-03 14:39:20 +0700

    fix: password integrity and prisma config

--- packages/db/prisma.config.ts
+++ packages/db/prisma.config.ts
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 import { defineConfig, env } from "prisma/config";
 
 dotenv.config({
-	path: "../../apps/server/.env",
+	path: "../../.env",
 });
 
 export default defineConfig({
